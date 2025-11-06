cd3281db5d3486bc5091ac93c90a305a47f4a067
vivek prajapati <vivek011prajapati@gmail.com>
2025-11-06 23:06:48 +0530

    fix: update MongoDB connection string and improve logging in db.js

--- db.js
+++ db.js
@@ -1,18 +1,18 @@
 import { MongoClient } from "mongodb";
 
 export const client = new MongoClient(
-  "mongodb://admin:admin@localhost:27017/todoApps?authSource=admin"
+  "mongodb://admin:admin@127.0.0.1:27017/todoApp?authSource=admin"
 );
 
 export async function connectDB() {
   await client.connect();
-  const db = client.db();
-  console.log("Database connected");
+  const db = client.db("todoApp");
+  console.log("✅ Authenticated & connected to todoApp DB");
   return db;
 }
 
 process.on("SIGINT", async () => {
   await client.close();
-  console.log("Client Disconnected!");
+  console.log("Client disconnected!");
   process.exit(0);
 });
