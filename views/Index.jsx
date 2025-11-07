7be51c9b8a2ed8d26ee3ae598f74160e70b40028
vivek prajapati <vivek011prajapati@gmail.com>
2025-11-07 08:35:47 +0530

    feat: implement todo list rendering with form for adding new todos

--- views/Index.jsx
+++ views/Index.jsx
@@ -1,12 +1,25 @@
 import React from "react";
-const Index = () => {
-    return (
-        <div>
-            <h1>
-                hii
-            </h1>
-        </div>
-    )
+import Layout from "./Layout";
 
+export default function ({ todos }) {
+  return (
+    <Layout title="Todo App">
+      <form action="/todos" method="post">
+        <input type="text" name="title" required />
+        <button>Add Todo</button>
+      </form>
+      <ul>
+        {todos.reverse().map(({ title, completed, _id }) => (
+          <li key={_id.toString()}>
+            <span
+              style={{ textDecoration: completed ? "line-through" : "none" }}
+            >
+              {title}
+            </span>{" "}
+            <button data-id={_id.toString()}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </Layout>
+  );
 }
-export default  Index;
