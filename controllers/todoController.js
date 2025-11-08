b34773b9d277020038c219b0d8dc08c91473d9ae
vivek prajapati <vivek011prajapati@gmail.com>
2025-11-08 12:20:38 +0530

    refactor: update todoController to use Todo model methods for database operations

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,65 +1,61 @@
 import { ObjectId } from "mongodb";
+import Todo from "../models/todoModel.js";
+
 export const addTodo = async (req, res) => {
   const todo = req.body;
   try {
-    await req.db.collection("todos").insertOne({...todo, completed : todo.completed || false});
+    await Todo.insertOne(todo);
     res.redirect("/todos");
   } catch (error) {
-    console.error(error);
+    console.log(error);
     res.status(500).json({ error: "Failed to create todo" });
   }
 };
 
-export const getAllTodo = async (req, res) => {
+export const getAllTodos = async (req, res) => {
   try {
-    const todos = await req.db.collection("todos").find().toArray();
-    res.render("Index.jsx", {todos})
+    const todos = await Todo.find();
+    res.render("index", { todos });
   } catch (error) {
+    console.log(error);
     res.status(500).json({ error: "Failed to fetch todos" });
   }
 };
 
 export const getTodoById = async (req, res) => {
   try {
-    const todo = await req.db
+    const todos = await req.db
       .collection("todos")
       .findOne({ _id: new ObjectId(req.params.id) });
-
-    if (!todo) return res.status(404).json({ error: "Todo not found" });
-    res.status(200).json(todo);
+    res.status(200).json(todos);
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Failed to fetch todo" });
+    console.log(error);
+    res.status(500).json({ error: "Failed to fetch todos" });
   }
 };
 
-export const updateTodo =  async (req, res) => {
+export const updateTodo = async (req, res) => {
   const { id } = req.params;
   const updatedTodo = req.body;
   try {
-    const result = await req.db
-      .collection("todos")
-      .updateOne({ _id: new ObjectId(id) }, { $set: updatedTodo });
-
-    if (result.modifiedCount === 0) {
-      return res.status(404).json({ error: "Todo not updated" });
-    }
+    await Todo.findByIdAndUpdate(id, updatedTodo);
+    // if (result.modifiedCount === 0) {
+    //   return res.status(404).json({ error: "Not updated" });
+    // }
     res.status(200).json({ message: "Todo updated successfully" });
   } catch (error) {
     res.status(500).json({ error: "Failed to update todo" });
   }
 };
 
-export const dltTodoById = async (req, res) => {
+export const deleteTodo = async (req, res) => {
   const { id } = req.params;
   try {
-    const result = await req.db
-      .collection("todos")
-      .deleteOne({ _id: new ObjectId(id) });
-
-    if (result.deletedCount === 0) {
-      return res.status(404).json({ error: "Todo not found" });
-    }
+    const result = await Todo.findByIdAndDelete(id);
+    console.log(result);
+    // if (result.deletedCount === 0) {
+    //   return res.status(404).json({ error: "Todo not found" });
+    // }
     res.status(200).json({ message: "Todo deleted successfully" });
   } catch (error) {
     res.status(500).json({ error: "Failed to delete todo" });
